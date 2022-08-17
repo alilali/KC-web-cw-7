7d757a10a9915c1774715ac23c7c14b4a51c3093
علي جعفر العلي <109864451+alilali@users.noreply.github.com>
2022-08-17 19:54:04 +0300

    Writting some codes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function status (bmi) {
+      if (bmi < 18.5) {
+    return `نقص بالوزن`
 
+    }else if (bmi >= 25) {
+
+    return `وزنك صحي`
+} else if (bmi <=25) 
+    return ` لديك زيادة الوزن`.style.color(blue)
+ 
+}
+
+function calulate () {
+
+
+let weight = document.getElementById(`Weight`)
+let Height = document.getElementById(`Height`)
+
+let bmi = BMI(Wight.Height)
+let desc = Status(bmi)
+
+let dessc = Status(bmi)
+
+let div = document.getElementById(`divtag`)
+
+
+div.innerTEXT = bmi + ` == ` + desc
+}
